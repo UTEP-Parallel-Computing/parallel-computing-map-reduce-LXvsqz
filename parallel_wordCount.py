e6af6b5c3567e589ad8b5dd9f4eb1dba1d27fb89
LXvsqz <avasquez31@miners.utep.edu>
2020-02-22 12:04:10 -0700

    I am pushing this so you can see how i got the average time to read a file and add it to a shared dictionary.

--- parallel_wordCount.py
+++ parallel_wordCount.py
@@ -12,7 +12,7 @@ for j in range(1,9): # poulate list holding files
     list_of_files.append(file)
 
 
-#-------------------------------------------------------------------------------------##
+#--------------------------------start parallel function-----------------------------------------------------##
 
 def parallelWC():
     
@@ -22,32 +22,43 @@ def parallelWC():
     with pymp.Parallel(8) as p:
         
         finalRT = int()
-        finalWC = int()  #FIGURING OUT TIME
-        wordTime = int()
         startReadTime = int()
+        
+        testingLoadTime = float()
+        
+        testingReadTime = float()
+        
+        averageReadTime = float()
         
         sumLock = p.lock
     
         for word in wordKey: # for every word in key, initialize that word in dictionary to 0
             wc[word] = 0
         
+        startReadTime = time.time()
+        
+        testingLoadTime = time.time()
         for file in p.iterate(list_of_files): # for each file, do this
             
             #file_read_time = time.time()
             
             text_file = open(file, "r")  #read mode
             
-            startReadTime = time.time()
+            #startReadTime = time.time()
             
             for line in text_file: # for every line in file. Note doing it this way instead of using re library so i can look for every word in each file
-            
+                
                 line = line.strip() 
                 line = re.sub(r'[^\w\s]','', line) # remove punctuation
                 line = line.casefold() # make everything lowercase
                 
                 words = line.split(" ") # split  by empty string
                
-               
+                testingReadTime = time.time() - testingLoadTime
+                
+                #print(testingReadTime)
+                averageReadTime = testingReadTime / 8
+                #print("-----", averageReadTime)
                 for word in words: #for each word in list of words
                     if word in wordKey:
                         sumLock.acquire() # lock any other thread from accessing shared dictionary
@@ -58,12 +69,16 @@ def parallelWC():
         finalRT = time.time() - startReadTime
     total_time = time.time() - start_time
     
+    
+    #finalRT = finalRT * 8
+    
+    print(averageReadTime)
     print("Total Operation time: " , total_time)
-    #print("Word Count time: " , finalWC)
-    print("File Reading time: " , finalRT)
+    print("Average Time to Read file: " , finalRT / 8)
+    print("Average Processing Time: " , total_time - finalRT)
+    print("--------------------------------------------")
     print(wc)
-#------------------------------------------------------------------------------------#
+#-----------------------------------end function-------------------------------------------------#
 
 parallelWC()
-
 
